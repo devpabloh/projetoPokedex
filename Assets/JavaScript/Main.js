46db3c176bc2bbf5a7c60e33d386c5a4819ea869
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-03-30 10:10:05 -0300

    atualização

--- Assets/JavaScript/Main.js
+++ Assets/JavaScript/Main.js
@@ -3,17 +3,34 @@ const limit = 10
 
 const url = ` http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response)=> response.json)
-    .then(function(jsonBody){
-        console.log(jsonBody)
-    })
-    .catch(function(error){
-        console.error(error)
-    })
-    .finally(function(){
-        console.log('Requisição concluída')
-    })
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+    
+            <div class="detail">
+                <ol class="types">
+                     <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
 
-const x = 10 + 10
-console.log(x)
+const pokemonList = document.getElementById(`pokemonList`)
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody)=> jsonBody.results)
+    .then((pokemons) => {
+        for(let i = 0; i < pokemons.length; i++){
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+
+        
+    })
+    .catch((error) => console.error(error))
