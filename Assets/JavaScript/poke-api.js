594f9b6eb877665cc4d599237debf819480c16d0
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-01 17:24:08 -0300

    atualização

--- Assets/JavaScript/poke-api.js
+++ Assets/JavaScript/poke-api.js
@@ -6,17 +6,17 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     pokemon.name = pokeDetail.name
     
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    
-    pokemon.types = 
-    pokemon.type = pokemon.types.get(a)
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 }
 
 pokeApi.getpokemonDetail = (pokemon)=> {
     return fetch(pokemon.url)
         .then((response)=> response.json())
-        .then((pokemon) => {
-
-        })
+        .then(convertPokeApiDetailToPokemon)
 }           
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -25,7 +25,7 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))  
+    .then((pokemons) => pokemons.map(pokeApi.getpokemonDetail))  
     .then((detailRequests)=> Promise.all(detailRequests))
     .then((pokemonsDetails)=>pokemonsDetails)
 } 
