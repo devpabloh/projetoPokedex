ebfffeb5071bb2b7ee45c10edae187f5ec1bfee7
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-01 09:42:55 -0300

    atualização

--- Assets/JavaScript/poke-api.js
+++ Assets/JavaScript/poke-api.js
@@ -1,12 +1,17 @@
 const pokeApi = {}
 
+pokeApi.getpokemon = (pokemon)=> {
+    return fetch(pokemon.url)
+        .then((response)=>response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = ` http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody)=> jsonBody.results)
-    .then((pokemons)=> pokemons.map((pokemon) => fetch(pokemon.url).then((response)=>response.json())))  
+    .then((pokemons)=> pokemons.map(pokeApi.getPokemonDetail))  
     .then((detailRequest)=> promisse.all(detailRequest))
     .then((pokemonsDetails)=>{
         debugger
