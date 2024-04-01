d68a019f876cc51f4fb48ba6974fab3fdc376d12
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-04-01 17:14:54 -0300

    atualização

--- Assets/JavaScript/poke-api.js
+++ Assets/JavaScript/poke-api.js
@@ -1,16 +1,31 @@
 const pokeApi = {}
 
-pokeApi.getpokemonDetail = (pokemon)=> {
-    return fetch(pokemon.url).then((response)=> response.json())
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemmon = new pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    
+    pokemon.types = 
+    pokemon.type = pokemon.types.get(a)
 }
+
+pokeApi.getpokemonDetail = (pokemon)=> {
+    return fetch(pokemon.url)
+        .then((response)=> response.json())
+        .then((pokemon) => {
+
+        })
+}           
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = ` http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
     .then((response) => response.json())
-    .then((jsonBody)=> jsonBody.results)
-    .then((pokemons)=> pokemons.map(pokeApi.getPokemonDetail))  
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))  
     .then((detailRequests)=> Promise.all(detailRequests))
     .then((pokemonsDetails)=>pokemonsDetails)
 } 
